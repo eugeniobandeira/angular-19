4b1cf8c07b0d7acf73cadf61ef8ae1da0f48988e
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-01-24 20:24:54 -0300

    refactor category component to remove unused imports and optimize change detection

--- crud-angular-19-master/src/app/features/categories/view/category/category.component.ts
+++ crud-angular-19-master/src/app/features/categories/view/category/category.component.ts
@@ -1,21 +1,17 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ColorsListComponent } from '../../components/colors-list/colors-list.component';
 import { MainListComponent } from "../../components/main-list/main-list.component";
-import { CategoryService } from '../../services/category.service';
-import { AsyncPipe } from '@angular/common';
 
 const COMPONENTS = [MainListComponent, ColorsListComponent];
-const PIPES = [AsyncPipe]
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss'],
   standalone: true,
-  imports: [...COMPONENTS, ...PIPES],
+  imports: [...COMPONENTS],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CategoryComponent {
   
-  private readonly categoryService = inject(CategoryService);
 }
