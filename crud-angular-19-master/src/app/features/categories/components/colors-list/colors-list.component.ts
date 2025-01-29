e2b43e4b34410fde9301b0fbc8a999647a85a9c7
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-01-29 18:52:02 -0300

    refactor ColorsListComponent to use consistent naming for category background colors

--- crud-angular-19-master/src/app/features/categories/components/colors-list/colors-list.component.ts
+++ crud-angular-19-master/src/app/features/categories/components/colors-list/colors-list.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { CategoryService } from '../../services/category.service';
-import { CATEGORY_BACKGROUND_COLORS } from '../../constants/category-colors';
+import { category_background_colors } from '../../constants/category-colors';
 
 const MODULES = [MatDividerModule];
 
@@ -17,5 +17,5 @@ export class ColorsListComponent {
   private readonly categoryService = inject(CategoryService);
 
   public categories = this.categoryService.categories;
-  public categoryBackgroundColors = CATEGORY_BACKGROUND_COLORS;
+  public categoryBackgroundColors = category_background_colors;
 }
