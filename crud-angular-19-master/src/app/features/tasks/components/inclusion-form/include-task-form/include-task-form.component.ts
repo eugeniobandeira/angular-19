375f7c9aa660d03bce897be01b6d27e4850b8f28
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-01-30 20:11:37 -0300

    refactor inclusion form component; add loading state handling and snackbar notifications

--- crud-angular-19-master/src/app/features/tasks/components/inclusion-form/include-task-form/include-task-form.component.ts
+++ crud-angular-19-master/src/app/features/tasks/components/inclusion-form/include-task-form/include-task-form.component.ts
@@ -1,6 +1,7 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   DestroyRef,
   inject,
 } from '@angular/core';
@@ -14,6 +15,10 @@ import { createTaskForm } from '../../../view/task/constants/create-task-form';
 import { TaskService } from '../../../services/task.service';
 import { ITask } from '../../../model/task.model';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { delay } from 'rxjs/internal/operators/delay';
+import { finalize } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { SnackbarService } from '../../../../../shared/services/snackbar.service';
 
 const MODULES = [
   MatFormFieldModule,
@@ -24,20 +29,37 @@ const MODULES = [
   ReactiveFormsModule,
 ];
 
+const COMMON = [CommonModule];
+
 @Component({
   selector: 'app-include-task-form',
   templateUrl: './include-task-form.component.html',
   styleUrls: ['./include-task-form.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [...MODULES],
+  imports: [...MODULES, ...COMMON],
 })
 export class IncludeTaskFormComponent {
   private readonly categoryService = inject(CategoryService);
-  private readonly taskService = inject(TaskService);
-  public destroy$ = inject(DestroyRef);
 
-  public categories = this.categoryService.categories;
+  public readonly taskService = inject(TaskService);
+
+  public readonly snackbarService = inject(SnackbarService);
+
+  public readonly destroy$ = inject(DestroyRef);
+
+  public readonly categories = this.categoryService.categories;
+
+  public isIncludeTaskFormDisabled = computed(() => {
+    if (this.taskService.isLoading()) {
+      this.newTaskForm.disable();
+
+      return this.taskService.isLoading();
+    }
+
+    this.newTaskForm.enable();
+    return this.taskService.isLoading();
+  });
 
   public selectionChangeHandler(event: MatSelectChange): void {
     const categoryId = event.value;
@@ -50,6 +72,8 @@ export class IncludeTaskFormComponent {
   public onAdd(): void {
     if (this.newTaskForm.invalid) return;
 
+    this.taskService.isLoading.set(true);
+
     const { title, categoryId } = this.newTaskForm.value;
 
     const newTask: Partial<ITask> = {
@@ -60,15 +84,21 @@ export class IncludeTaskFormComponent {
 
     this.taskService
       .create(newTask)
-      .pipe(takeUntilDestroyed(this.destroy$))
+      .pipe(delay(4000), 
+        finalize(() => this.taskService.isLoading.set(false)),
+        takeUntilDestroyed(this.destroy$))
       .subscribe({
         next: task => this.taskService.updateTaskInList(task),
         error: error => {
-          console.error('Error creating task:', error);
+          this.snackbarConfigHandler(error.message);
         },
         complete: () => {
-          alert('Task created successfully');
+          this.snackbarConfigHandler('Task added successfully');
         },
       });
+  }
+
+  public snackbarConfigHandler(message: string): void {
+    this.snackbarService.showSnackBar(message, 4000, 'end', 'bottom');
   }
 }
