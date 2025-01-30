e56adef121f7151d7fcd5366d100985578dbfaeb
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-01-30 20:11:50 -0300

    add loading state handling to inclusion form; update task service injection and component imports

--- crud-angular-19-master/src/app/features/tasks/components/inclusion-form/inclusion-form.component.ts
+++ crud-angular-19-master/src/app/features/tasks/components/inclusion-form/inclusion-form.component.ts
@@ -2,8 +2,11 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { IncludeTaskFormComponent } from './include-task-form/include-task-form.component';
 import { CategoryService } from '../../../categories/services/category.service';
 import { categoryId_background_colors } from '../../../categories/constants/category-colors';
+import { CommonModule } from '@angular/common';
+import { TaskService } from '../../services/task.service';
 
 const COMPONENTS = [IncludeTaskFormComponent];
+const COMMON = [CommonModule];
 
 @Component({
   selector: 'app-inclusion-form',
@@ -11,10 +14,11 @@ const COMPONENTS = [IncludeTaskFormComponent];
   styleUrls: ['./inclusion-form.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [...COMPONENTS]
+  imports: [...COMPONENTS, ...COMMON]
 })
 export class InclusionFormComponent { 
   private readonly categoryService = inject(CategoryService);
+  public readonly taskService = inject(TaskService);
 
   public readonly selectedCategoryId = this.categoryService.selectedCategoryId;
 
