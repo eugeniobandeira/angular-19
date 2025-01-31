4e8438aa6da44dc7c9b3b89faf2c02094c9bc560
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-01-30 21:18:52 -0300

    refactor inclusion form component tests; simplify async setup and enhance module imports

--- crud-angular-19-master/src/app/features/tasks/components/inclusion-form/inclusion-form.component.spec.ts
+++ crud-angular-19-master/src/app/features/tasks/components/inclusion-form/inclusion-form.component.spec.ts
@@ -1,20 +1,25 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InclusionFormComponent } from './inclusion-form.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 describe('InclusionFormComponent', () => {
   let component: InclusionFormComponent;
   let fixture: ComponentFixture<InclusionFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ InclusionFormComponent ]
-    })
-    .compileComponents();
-  }));
+      imports: [InclusionFormComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideAnimations(),
+      ],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InclusionFormComponent);
