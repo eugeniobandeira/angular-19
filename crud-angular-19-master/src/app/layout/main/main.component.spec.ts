a14c89b281a211ae753cedcf01e8f075a5ca09fb
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-01-30 21:19:07 -0300

    add animations provider to MainComponent tests; enhance testing environment setup

--- crud-angular-19-master/src/app/layout/main/main.component.spec.ts
+++ crud-angular-19-master/src/app/layout/main/main.component.spec.ts
@@ -3,15 +3,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MainComponent } from './main.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       imports: [MainComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [provideHttpClient(), provideHttpClientTesting(), provideAnimations()],
     })
     .compileComponents();
 
